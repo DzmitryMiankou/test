9cb10ea3f4743428e425e1e8a61b685de41675c7
DzmitryMiankou <gmiankou@gmail.com>
2023-06-07 22:24:12 +0300

    rerr

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, } from '@mui/material';
 import NowDate from './nowDate/NowDate';
+import AnalyticsCalls from './analyticsCalls/AnalyticsCalls';
 
 const styleSX = {
     box: {
@@ -9,11 +10,13 @@ const styleSX = {
         backgroundColor: "var(--white)",
         width: "100%",
         display: "flex",
-        alignItems: "center",
         justifyContent: "center",
     },
     box_width: {
         width: "var(--indentSide)",
+        display: "flex",
+        alignItems: "center",
+        gap: "8%"
     }
 };
 
@@ -22,6 +25,7 @@ const Header = () => {
         <Box sx={styleSX.box}>
             <Box sx={styleSX.box_width}>
                 <NowDate />
+                <AnalyticsCalls />
             </Box>
         </Box>
     );
---
+++ src/components/header/analyticsCalls/AnalyticsCalls.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Box, Typography } from '@mui/material';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+
+const data = [
+    { text: "Новые звонки", statiscticaStart: 30, statiscticaEnd: 20, color: "var(--green-analytic)" },
+    { text: "Качество разговоров", statisctica: 40, color: "var(--green-yellow)" },
+    { text: "Конверсия в заказ", statisctica: 67, color: "var(--green-red)" },
+];
+
+const AnalyticsCalls = () => {
+    return (
+        <Box sx={{ display: "flex", gap: "8%", whiteSpace: "nowrap", }}>{
+            data.map(({ text, statisctica, color, statiscticaStart, statiscticaEnd }, i) =>
+                <Box key={i}>
+                    <Typography sx={{ fontSize: "14px !important", }}>
+                        {text}{" "}
+                        <Box component="span" sx={{ color: color }}>{statisctica ? statisctica : `${statiscticaEnd} из ${statiscticaStart} шт`}</Box>
+                        <>{statisctica ? <Box component="span" sx={{ color: color }}>%</Box> : <></>}</>
+                    </Typography>
+                    <LinearProgress variant="determinate" value={statisctica ? statisctica : Math.floor((Number(statiscticaEnd) / Number(statiscticaStart)) * 100)} sx={(theme) => ({
+                        height: 6,
+                        borderRadius: 5,
+                        maxWidth: "156px",
+                        [`&.${linearProgressClasses.colorPrimary}`]: {
+                            backgroundColor: "#DEE6F5",
+                        },
+                        [`& .${linearProgressClasses.bar}`]: {
+                            borderRadius: 5,
+                            backgroundColor: color,
+                        },
+                    })} />
+                </Box>
+            )}
+        </Box>
+    );
+}
+
+export default AnalyticsCalls;
